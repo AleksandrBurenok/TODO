5fbf7cad48bb2b47d7cef341e97766bff27f9cd0
Aleksandr <oleksandrburenok@gmail.com>
2019-02-19 16:36:40 +0200

    something changes

---
+++ todo-app/src/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button className="btn">Button</button>
+        )
+    }
+}
+
+export default Button;
